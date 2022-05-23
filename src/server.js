4973ff8493377c6f13fd313c06bd60f5c399d182
Marius <marius@example.com>
2022-05-23 09:41:20 +0300

    static dir, css, img

--- src/server.js
+++ src/server.js
@@ -4,10 +4,19 @@ const path = require('path');
 
 const PORT = 3000;
 
+// static directory
+const staticDir = path.join(__dirname, 'assets');
+app.use(express.static(staticDir));
+
 app.get('/', function (req, res) {
   const pathToIndex = path.join(__dirname, 'views', 'index.html');
   console.log(__dirname);
   res.sendFile(pathToIndex);
+});
+
+app.get('/about', (req, res) => {
+  const pathToAbout = path.join(__dirname, 'views', 'about.html');
+  res.sendFile(pathToAbout);
 });
 
 app.listen(PORT, () => console.log('listening on port', PORT));
